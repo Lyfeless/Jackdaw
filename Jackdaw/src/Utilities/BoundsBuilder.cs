8831409f6e8be0095373dbbc92df2f64a7c4c50f
Lyfeless <alexweinfurter@gmail.com>
2026-02-04 23:18:36 -0600

    Simplify boundsbuilder logic

--- Jackdaw/src/Utilities/BoundsBuilder.cs
+++ Jackdaw/src/Utilities/BoundsBuilder.cs
@@ -7,22 +7,12 @@ namespace Jackdaw;
 /// Simple utility to building a bounding box out of many points and rectangles.
 /// </summary>
 public struct BoundsBuilder {
-    Vector2 min = Vector2.Zero;
-    Vector2 max = Vector2.Zero;
-
-    int PointCount = 0;
+    bool hasPoints = false;
 
     /// <summary>
     /// The total bounds all the added values cover.
     /// </summary>
-    public Rect Rect;
-
-    /// <summary>
-    /// Create a new empty builder.
-    /// </summary>
-    public BoundsBuilder() {
-        SetRect();
-    }
+    public Rect Rect = new(0, 0);
 
     /// <summary>
     /// Create a new builder the a starting size of a point.
@@ -59,64 +49,46 @@ public struct BoundsBuilder {
     /// <summary>
     /// Add a single point to the builder's bounds
     /// </summary>
-    /// <param name="point">the point to add.</param>
+    /// <param name="point">The point to add.</param>
     /// <returns>The expanded builder.</returns>
     public BoundsBuilder Add(Vector2 point) {
-        if (PointCount == 0) {
-            min = point;
-            max = point;
-        }
-        else {
-            min = Vector2.Min(min, point);
-            max = Vector2.Max(max, point);
-        }
+        if (!hasPoints) { Rect = new(point, Vector2.Zero); }
+        else { Rect = Rect.Conflate(point); }
 
-        PointCount++;
-        SetRect();
+        hasPoints = true;
         return this;
     }
 
     /// <summary>
     /// Add a single rect to the builder's bounds
     /// </summary>
-    /// <param name="rect">the rect to add.</param>
+    /// <param name="rect">The rect to add.</param>
     /// <returns>The expanded builder.</returns>
     public BoundsBuilder Add(Rect rect) {
-        if (PointCount == 0) {
-            min = rect.TopLeft;
-            max = rect.BottomRight;
-        }
-        else {
-            min = Vector2.Min(min, rect.TopLeft);
-            max = Vector2.Max(max, rect.BottomRight);
-        }
+        if (!hasPoints) { Rect = rect; }
+        else { Rect = Rect.Conflate(rect); }
 
-        PointCount += 4;
-        SetRect();
+        hasPoints = true;
         return this;
     }
 
     /// <summary>
     /// Add multiple points to the builder's bounds
     /// </summary>
-    /// <param name="points">the points to add.</param>
+    /// <param name="points">The points to add.</param>
     /// <returns>The expanded builder.</returns>
     public BoundsBuilder Add(params Vector2[] points) {
-        foreach (Vector2 point in points) {
-            Add(point);
-        }
+        foreach (Vector2 point in points) { Add(point); }
         return this;
     }
 
     /// <summary>
     /// Add multiple rects to the builder's bounds
     /// </summary>
-    /// <param name="rects">the rects to add.</param>
+    /// <param name="rects">The rects to add.</param>
     /// <returns>The expanded builder.</returns>
     public BoundsBuilder Add(params Rect[] rects) {
-        foreach (Rect rect in rects) {
-            Add(rect);
-        }
+        foreach (Rect rect in rects) { Add(rect); }
         return this;
     }
 
@@ -125,15 +97,8 @@ public struct BoundsBuilder {
     /// </summary>
     /// <returns>The empty builder.</returns>
     public BoundsBuilder Clear() {
-        min = Vector2.Zero;
-        max = Vector2.Zero;
-        PointCount = 0;
-
-        SetRect();
+        hasPoints = false;
+        Rect = new(0, 0);
         return this;
-    }
-
-    void SetRect() {
-        Rect = PointCount > 0 ? new(min, max - min) : new(0, 0, 0, 0);
     }
 }
