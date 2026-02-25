dee3e767145c9a178a418218434a99e43d73e6a3
Lyfeless <alexweinfurter@gmail.com>
2026-02-25 14:45:05 -0600

    Scissor render action updates

--- Jackdaw/src/RenderAction/Scissor.cs
+++ Jackdaw/src/RenderAction/Scissor.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Foster.Framework;
 
 namespace Jackdaw;
@@ -5,12 +6,22 @@ namespace Jackdaw;
 /// <summary>
 /// A render action that applies a scissor to clip the rendering of all components and children into a rectangle.
 /// </summary>
-/// <param name="scissor">The clip bounds.</param>
-public class RenderActionScissor(RectInt scissor) : ActorRenderAction() {
-    public RectInt Scissor = scissor;
+/// <param name="bounds">The clip bounds.</param>
+public class RenderActionScissor(BoundsComponent bounds) : ActorRenderAction() {
+    /// <summary>
+    /// The clip bounds.
+    /// </summary>
+    public BoundsComponent Bounds = bounds;
+
+    /// <summary>
+    /// If the scissor should be relative to the actor it's attached to.
+    /// </summary>
+    public bool Relative = true;
 
     public override void PreRender(RenderActionContainer container) {
-        container.CurrentBatcher.PushScissor(Scissor);
+        Rect rect = Bounds.Rect;
+        if (Relative) { rect = rect.Translate(container.Owner.GlobalPosition); }
+        container.CurrentBatcher.PushScissor(rect.Int());
     }
 
     public override void PostRender(RenderActionContainer container) {
